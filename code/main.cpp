2dd6d3dfb1e98262053b15e4fc469a1c746868f2
Chalearm Saelim <chalearm33@hotmail.com>
2022-04-10 20:56:38 +0700

    Added makefile and update the code in main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include "game.h"
 using namespace std;
 
 int main(int argc, char **argv)
 {
     cout << "hello world \n";
+    game aGame;
+    aGame.runGame();
     return 0;
 }
